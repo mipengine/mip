4665bea71e4246533e825d1595660e8c36c35c30
JoeRay61 <joeray199261@gmail.com>
2016-07-07 19:01:15 +0800

    add mip-iframe

--- dist/mip_ad.js
+++ dist/mip_ad.js
@@ -1,4 +1,184 @@
-define("mip_ads_register",["require","viewport","buildins/registerMipElement","ads/mip-ad"],function(i){var t=i("viewport");i("buildins/registerMipElement"),window.registerMipElement("mip-ad",i("ads/mip-ad")),$(function(){window.setTimeout(function(){$(".mip-element").each(function(){this.inviewCallback()})},100)}),t.onScroll(function(){$(".mip-element").each(function(){this.inviewCallback()})})}),define("ads/mip-ad",["require","buildins/customElement"],function(i){function t(){if(!this.isRender){this.isRender=!0;
-var i=this.getAttribute("tpl");switch(i){case"onlyImg":e.call(this);break;case"noneImg":s.call(this);break;case"oneImg":a.call(this);break;case"moreImg":n.call(this)}}}function e(){var i=$(this),t=this.getAttribute("src"),e=i.data("img"),s=i.data("size").trim().split(" "),a=s[0],n=s[1],c=(n/a*100).toFixed(2),l=['<a href="'+t+'" class="c-urljump">','<div style="position:relative;width:100%;padding-bottom:'+c+'%;overflow:hidden;">','<img src="'+e+'" style="position:absolute;left:0;top:0;width:100%;">',"</div>","</a>"].join("");
-i.append(l)}function s(){var i=$(this),t=this.getAttribute("src"),e=(i.data("img"),i.data("size").trim().split(" ")),s=e[0],a=e[1],n=((a/s*100).toFixed(2),i.data("title")),c=['<a class="c-blocka" href="'+t+'" class="c-urljump" style="padding: 0 21px;font-size: 18px;line-height: 26px;">','<div class="c-row">','<div class="c-span12">','<div class="sf-news-feed-content">','<div class="c-line-clamp2">',"<span>"+n+"</span>","</div>","</div>","</div>","</div>","</a>"].join("");i.append(c)}function a(){var i=$(this),t=this.getAttribute("src"),e=i.data("img"),s=i.data("size").trim().split(" "),a=s[0],n=s[1],c=((n/a*100).toFixed(2),i.data("title")),l=['<a class="c-blocka" href="'+t+'" class="c-urljump" style="padding: 0 21px;font-size: 18px;line-height: 26px;">','<div class="c-row" style="-webkit-box-align: center;-webkit-align-items: center;">','<div class="c-span4">','<div class="c-img c-img-x">','<img src="'+e+'">',"</div>","</div>",'<div class="c-span8">',"<div>",'<div class="c-line-clamp2">',"<span>"+c+"</span>","</div>","</div>","</div>","</div>","</a>"].join("");
-i.append(l)}function n(){}var c=i("buildins/customElement");return c.prototype.init=function(){this.build=t},c}),require(["mip_ads_register"]);
+/**
+ * @file MIP_ADS_REGISTER: 广告组件注册器
+ * @author wangpei07@baidu.com
+ * @version 1.0
+ * @copyright 2016 Baidu.com, Inc. All Rights Reserved
+ */
+
+define('mip_ads_register', ['require', 'viewport', 'buildins/registerMipElement', 'ads/mip-ad'], function(require){
+    /**
+    * 初始化相关JS
+    */
+
+    /**
+     *  初始化视图
+     * */
+    var viewport = require('viewport');
+
+
+    /**
+     *  web compenent组件初始化
+     * */
+    require('buildins/registerMipElement');
+
+    /*
+     * 注册mip-ad 组件
+     */
+    window.registerMipElement('mip-ad', require('ads/mip-ad'));
+
+    //页面初始化后，处理可视区域内元素
+    $(function(){
+        window.setTimeout(function(){
+            $('.mip-element').each(function(){
+                this.inviewCallback();
+            });
+        },100);
+    });
+
+    //元素绑定scroll，用于lazy load等场景
+    viewport.onScroll(function(){
+        $('.mip-element').each(function(){
+            this.inviewCallback();
+        });
+    });
+
+});
+;
+/**
+* 广告插件
+* @exports modulename
+* @author chenrui09@baidu.com
+* @version 1.0
+* @copyright 2016 Baidu.com, Inc. All Rights Reserved
+*/
+
+define('ads/mip-ad', ['require', 'buildins/customElement'], function(require){
+    var customElem = require('buildins/customElement');
+    
+    function render() {
+        if (this.isRender) {
+            return;
+        }
+
+        this.isRender = true;
+
+        var tpl = this.getAttribute('tpl');
+
+        switch(tpl) {
+            case 'onlyImg':
+                renderOnlyImg.call(this);
+                break;
+            case 'noneImg':
+                renderNoneImg.call(this);
+                break;
+            case 'oneImg':
+                renderOneImg.call(this);
+                break;
+            case 'moreImg':
+                renderMoreImg.call(this);
+                break;
+        }
+    }
+
+    function renderOnlyImg() {
+        var $this = $(this);
+
+        var url = this.getAttribute('src');
+        var img = $this.data('img');
+        var size = $this.data('size').trim().split(' ');
+        var width = size[0];
+        var height = size[1];
+        var ratio = (height/width*100).toFixed(2);
+
+        var html = [
+            '<a href="'+ url +'" class="c-urljump">',
+                '<div style="position:relative;width:100%;padding-bottom:'+ ratio +'%;overflow:hidden;">',
+                    '<img src="'+ img +'" style="position:absolute;left:0;top:0;width:100%;">',
+                '</div>',
+            '</a>'
+        ].join('');
+
+        $this.append(html);
+    }
+
+    function renderNoneImg() {
+    	var $this = $(this);
+
+        var url = this.getAttribute('src');
+        var img = $this.data('img');
+        var size = $this.data('size').trim().split(' ');
+        var width = size[0];
+        var height = size[1];
+        var ratio = (height/width*100).toFixed(2);
+        var title = $this.data('title');
+
+        var html = [
+            '<a class="c-blocka" href="'+ url +'" class="c-urljump" style="padding: 0 21px;font-size: 18px;line-height: 26px;">',
+                '<div class="c-row">',
+                    '<div class="c-span12">',
+                        '<div class="sf-news-feed-content">',
+                            '<div class="c-line-clamp2">',
+                                '<span>' + title + '</span>',
+                            '</div>',
+                        '</div>',
+                    '</div>',
+                '</div>',
+            '</a>'
+        ].join('');
+
+        $this.append(html);
+
+    }
+
+    function renderOneImg() {
+    	var $this = $(this);
+
+        var url = this.getAttribute('src');
+        var img = $this.data('img');
+        var size = $this.data('size').trim().split(' ');
+        var width = size[0];
+        var height = size[1];
+        var ratio = (height/width*100).toFixed(2);
+        var title = $this.data('title');
+
+        var html = [
+            '<a class="c-blocka" href="'+ url +'" class="c-urljump" style="padding: 0 21px;font-size: 18px;line-height: 26px;">',
+                '<div class="c-row" style="-webkit-box-align: center;-webkit-align-items: center;">',
+                    '<div class="c-span4">',
+                        '<div class="c-img c-img-x">',
+                            '<img src="'+ img +'">',
+                        '</div>',
+                    '</div>',
+                    '<div class="c-span8">',
+                        '<div>',
+                            '<div class="c-line-clamp2">',
+                                '<span>' + title + '</span>',
+                            '</div>',
+                        '</div>',
+                    '</div>',
+                '</div>',
+            '</a>'
+        ].join('');
+
+        $this.append(html);
+    }
+
+    function renderMoreImg() {
+
+    }
+
+    customElem.prototype.init = function() {
+
+        this.build = render;
+
+    };
+
+    return customElem;
+
+});
+
+;
+
+require(['mip_ads_register']);
+
+
