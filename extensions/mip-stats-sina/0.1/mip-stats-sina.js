19f0508c195084f5adc8e45566849506e4cc4cc0
Lingjun.Meng <junmer@foxmail.com>
2016-07-21 18:03:16 +0800

    fix elem name

--- extensions/mip-stats-sina/0.1/mip-stats-sina.js
+++ extensions/mip-stats-sina/0.1/mip-stats-sina.js
@@ -97,5 +97,5 @@ define(function() {
 require(['mip-stats-sina'], function (sina) {
     //注册组件
     MIP.registerMipElement('mip-sina-tongji', sina);
-    MIP.registerMipElement('mip-stats-sina', netease);
+    MIP.registerMipElement('mip-stats-sina', sina);
 });
