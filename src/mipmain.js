ddfd53b869e158b05979ff816416584492fefb73
qijian <qijian@baidu.com>
2016-07-26 11:29:16 +0800

    by qijian

---
+++ src/mipmain.js
@@ -0,0 +1,29 @@
+/* 主逻辑 */
+__inline('./deps/jquery.js');
+__inline('./deps/esl.js');
+__inline('./deps/esl_config.js');
+__inline('./deps/emit.js');
+__inline('./observable.js');
+__inline('./platform.js');
+__inline('./message_center.js');
+__inline('./gesture.js');
+__inline('./viewport.js');
+__inline('./deps/document-register-element.max.js');
+__inline('./customElement.js');
+__inline('./registerMipElement.js');
+__inline('./utils/util.js');
+__inline('./utils/cssLoader.js');
+
+
+/* 内置组件 */
+__inline('./builtins/mip-img.js');
+__inline('./builtins/mip-gif.js');
+__inline('./builtins/mip-pix.js');
+__inline('../extensions/mip-carousel.js');
+__inline('../extensions/mip-iframe.js');
+__inline('../extensions/recommend.js');
+__inline('./builtins/img-viewer.js');
+__inline('./builtins/mip-video.js');
+__inline('./builtins/video/player.js');
+__inline('./builtins/video/popup.js');
+__inline('./builtins/mip_builtins.js');
