1b7497be34f8b819b4ed4528c532010ad744cadd
lilangbo <lilangbo@baidu.com>
2016-07-27 14:57:55 +0800

    上线文档增加

--- assets/sidebar_node.js
+++ assets/sidebar_node.js
@@ -9,7 +9,7 @@ var config = {
       '1_join_guied':'接入指南',
       '2_dev_guied':'开发指南',
       '3_frame_guied':'框架基础',
-      '6_online':'上线'
+      '5_online':'上线'
     }
 };
 var fs = require('fs'),
