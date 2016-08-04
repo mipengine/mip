502456886f309dc0e85b067a9a535f72b5162bfc
System Administrator <root@MacBook-Pro-3.local>
2016-08-04 10:50:02 +0800

    更新全网推荐

--- extensions/ads/0.1/mip-ad-qwang.js
+++ extensions/ads/0.1/mip-ad-qwang.js
@@ -8,24 +8,18 @@
 
 
 define(function() {
-    var customElem = require('customElement');
 
-    function build() {
-        if (this.isRender) {
-            return;
-        }
+    function render(_this) {
 
-        this.isRender = true;
-
-        var cpropsid = this.getAttribute("cpro_psid");
-        var cpropswidth = this.getAttribute("cpro_pswidth")||"auto";
-        var cpropsheight = this.getAttribute("cpro_psheight")||"230";
+        var cpropsid = _this.getAttribute("cpro_psid");
+        var cpropswidth = _this.getAttribute("cpro_pswidth")||"auto";
+        var cpropsheight = _this.getAttribute("cpro_psheight")||"230";
 
         var scriptstr = 'var cpro_psid = "' +cpropsid+ '" ;var cpro_pswidth = "'+cpropswidth+ '";var cpro_psheight="' + cpropsheight + '";';
         var ele = document.createElement("script");
         ele.innerHTML = scriptstr;
-        this.appendChild(ele)
-
+       
+        _this.appendChild(ele)
         initJs();
     }
 
@@ -37,23 +31,19 @@ define(function() {
     function initJs() {
         var WEBADVISEJS = document.getElementById('MIP_WEBADVISE_JS');
         if(WEBADVISEJS) return;
+        
         var script = document.createElement('script');
         script.src = '//su.bdimg.com/static/dspui/js/um.js';
         script.id = "WEBADVISEJS";
+        
         document.body.appendChild(script);     
     }
 
-
-    customElem.prototype.init = function() {
-        this.build = build;
+    return {
+        render:render
     };
 
-    return customElem;
-
 });
 
-require(['mip-qwang'], function (qwang) {
-    MIP.registerMipElement('mip-qwang', qwang);
-});
 
 
