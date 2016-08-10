5dc20ea7243861fb10be1d796c541516ba21432f
System Administrator <root@MacBook-Pro-3.local>
2016-08-10 20:57:20 +0800

    update

--- extensions/mip-askad/0.1/mip-askad.js
+++ extensions/mip-askad/0.1/mip-askad.js
@@ -1,1 +1,180 @@
-define("extensions/mip-askad/0.1/mip-askad",["require","customElement"],function(t){function e(){if(!this.isRender){this.isRender=!0;var t=this,e=this.getAttribute("paramsid")||"",s=this.getAttribute("otherparamkey")||"0",a=this.getAttribute("otherparamvalue")||"",c=this.getAttribute("cboptions")||[],r=[];if(c)try{r=new Function("return "+c)()}catch(m){}var d=$(".g-under-ask h1"),u=$(".g-under-ask .crazy_keyword_inlink");if(0==d.length||0==u.length)var h="";else var h=d.html()+" "+u.html().slice(0,30);i(h,function(){o(),n(e,Number(s),a.toString(),r,t)})}}function i(t,e){for(var i=["//ip.120ask.com/lt?js=m.120ask.com&r=1470735643","//scws.120ask.com/scws?t=js&content="+t,"//m.120ask.com/pub/js/x_m_none_jquery.js"],n=3,o=0;o<i.length;o++){var s=document.createElement("script");$("#MIP-ASK-AD"+o).length>0&&$("#MIP-ASK-AD"+o).remove(),s.id="MIP-ASK-AD"+o,s.src=i[o];var a=document.head.firstChild;document.head.insertBefore(s,a),s.onload=function(){1==n&&e(),n-=1}}}function n(t,e,i,n,o){YWBD.prototype.YWBD_WRITE=function(t){var e=t.code;location.href.indexOf("mipcache.bdstatic.com")>=0&&(e=e.replace(/http:\/\/cdn.120askimages.com/g,"/i/cdn.120askimages.com")),$(o).append(e),n&&0!=n.length&&s(n)};var a=new YWBD;a.YWBD_SET_PARAMS("Id",t),a.YWBD_SET_OTHER_PARAM(e,i),a.YWBD_SET_AREA_PARAMS(),a.YWBD_SET_LOG(),a.YWBD_AD_AJAX($(o))}function o(){0==location.href.indexOf("https")&&(YWBD_LOG_URL.indexOf("eclick.120ask.com/lualog")>=0?YWBD_LOG_URL="//eclick.120ask.com/lualog":YWBD_LOG_URL.indexOf("scws.120ask.com")>=0?YWBD_LOG_URL="//gss2.bdstatic.com/5bUVf8T-RdYFkdTgoY3K":YWBD_LOG_URL.indexOf("cdn.120askimages.com")>=0&&(YWBD_LOG_URL="//gss2.bdstatic.com/9bA1smf7RAUTmdKfo9eSBHF6hhy"))}function s(t){for(var e=0;e<t.length;e++)"show"==t[e].type?c(t[e].target):"hide"==t[e].type?r(t[e].target):"remove"==t[e].type&&a(t[e].target)}function a(t){$(t).remove()}function c(t){$(t).show()}function r(t){$(t).hide()}var m=t("customElement");return m.prototype.init=function(){this.build=e},m}),require(["extensions/mip-askad/0.1/mip-askad"],function(t){MIP.registerMipElement("mip-askad",t)});
+/**
+ * @file 120ask广告组件
+ * @author fengchuantao
+ * @time 2016.8.8
+ */
+
+define(function() {
+    var customElem = require('customElement');
+
+    function build() {
+
+        if (this.isRender) {
+            return;
+        }
+        this.isRender = true;
+
+        var _this = this;
+        var  params = this.getAttribute("paramsid") || "";
+        var  otherparamkey = this.getAttribute("otherparamkey") || "0";
+        var  otherparamvalue = this.getAttribute("otherparamvalue") || "";
+        var callbackconfig = this.getAttribute("cboptions") || [] ;
+
+        //回调配置序列化
+        var callbackdata = [];
+        if (callbackconfig) {
+            try {
+                callbackdata = new Function('return ' + callbackconfig)()
+            } catch (e) {}
+        }
+
+
+        //根据页面获取推荐内容
+        var $title = $(".g-under-ask h1");
+        var $dse = $(".g-under-ask .crazy_keyword_inlink");
+        
+        if($title.length == 0 || $dse.length == 0) {
+            var content = "";
+        }else {
+            var content = $title.html() +" "+ $dse.html().slice(0,30);
+        }
+        
+        innerJs(content,function(){
+            replaceUrl();
+            getadDate(params,Number(otherparamkey),otherparamvalue.toString(),callbackdata,_this)
+        })
+    }
+
+    /**
+     * 加载120ask网站js
+     */
+    function innerJs(content,callback) {
+
+        var allurl = ["//ip.120ask.com/lt?js=m.120ask.com&r=1470735643","//scws.120ask.com/scws?t=js&content="+content,"//m.120ask.com/pub/js/x_m_none_jquery.js"];
+        var calbacklen = 3;
+
+        for(var i = 0; i< allurl.length; i++) {
+
+            var script = document.createElement("script");
+            
+            if($("#MIP-ASK-AD"+i).length > 0) {
+                $("#MIP-ASK-AD"+i).remove();
+            }
+
+            script.id = "MIP-ASK-AD"+i;
+            script.src = allurl[i];
+
+            var firstChild = document.head.firstChild;
+            document.head.insertBefore(script,firstChild);
+
+            script.onload = function() {
+                if(calbacklen == 1) {
+                    callback()
+                }
+                calbacklen  = calbacklen - 1;
+            }
+        }
+    }
+
+    /**
+     * 执行ask网站js获取广告数据
+     */
+    function getadDate(params,otherparamkey,otherparamvalue,callbackdata,layout) {
+
+        //重写YWBD原型方法以实现标签替换
+        YWBD.prototype.YWBD_WRITE =  function(backdata) {
+            var code = backdata.code;
+
+            if(location.href.indexOf('mipcache.bdstatic.com') >= 0 ) { //如果是mip页
+                code = code.replace(/http:\/\/cdn.120askimages.com/g,"/i/cdn.120askimages.com") //替换img为src代理路径
+            }
+
+            $(layout).append(code);
+            if(callbackdata&&callbackdata.length != 0 ) {
+                renderCallback(callbackdata)
+            }
+
+        }
+
+        //以下为广告投放原始代码
+        var smallad = new YWBD();
+        smallad.YWBD_SET_PARAMS('Id', params);
+        smallad.YWBD_SET_OTHER_PARAM(otherparamkey, otherparamvalue);
+        smallad.YWBD_SET_AREA_PARAMS();
+        smallad.YWBD_SET_LOG();
+        smallad.YWBD_AD_AJAX($(layout));
+    }
+
+    /**
+     * https环境下修正日志链接
+     */
+    function replaceUrl() {
+        if(location.href.indexOf('https') == 0) {
+            if(YWBD_LOG_URL.indexOf("eclick.120ask.com/lualog") >= 0) {
+
+                YWBD_LOG_URL = "//eclick.120ask.com/lualog";
+
+            }else if(YWBD_LOG_URL.indexOf("scws.120ask.com") >= 0) {
+
+                YWBD_LOG_URL = "//gss2.bdstatic.com/5bUVf8T-RdYFkdTgoY3K";
+
+            }else if (YWBD_LOG_URL.indexOf("cdn.120askimages.com") >= 0) {
+
+                YWBD_LOG_URL = "//gss2.bdstatic.com/9bA1smf7RAUTmdKfo9eSBHF6hhy";
+            }
+        }
+    }
+
+    /**
+     * 渲染回调
+     */
+    function renderCallback(callbackdata) {
+        for(var i = 0; i<callbackdata.length; i++ ) {
+            if(callbackdata[i].type == "show") {
+                showdom(callbackdata[i].target)
+            }else if(callbackdata[i].type == "hide") {
+                hidedom(callbackdata[i].target)
+            }else if(callbackdata[i].type == "remove") {
+                removedom(callbackdata[i].target)
+            }
+        }
+    }
+
+    /**
+     * 删除节点
+     */
+    
+    function removedom(obj) {
+        $(obj).remove();
+    }
+
+    /**
+     * 显示节点
+     */
+    
+    function showdom(obj) {
+        $(obj).show();
+    }
+
+    /**
+     * 隐藏节点
+     */
+    function hidedom(obj) {
+        $(obj).hide();
+    }
+    
+    
+        
+
+    customElem.prototype.init = function() {
+        this.build = build;
+    };
+
+    return customElem;
+
+});
+
+require(['mip-askad'], function (askad) {
+    MIP.registerMipElement('mip-askad', askad);
+});
+
