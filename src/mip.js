038a5362a62dd5fadca9742aa27a5cd34d683008
erik <erik168@163.com>
2016-10-13 14:59:20 +0800

    amdlize

--- src/mip.js
+++ src/mip.js
@@ -1,20 +1,59 @@
-// MIP initialization
-require(['./element', './components/index', 
-    './viewer', './viewport', 'resources', './util/animation'], 
-    function (registerMipElement, components, viewer, viewport, resources, animation) {
-    'use strict';
+define(function (require) {
+    require('zepto');
+    require('naboo');
+    require('spark');
+    require('fetch-jsonp');
 
-    
+
+    require('./util/fn');
+    require('./util/gesture/gesture-recognizer');
+    require('./util/gesture/data-processor');
+    require('./util/gesture');
+    require('./util/platform');
+    require('./util/event-emitter');
+    var animation = require('./util/animation');
+    require('./util/event-action');
+
+    /* dom */
+    require('./dom/css-loader');
+    require('./dom/rect');
+    require('./dom/event');
+    require('./dom/css');
+    require('./dom/dom');
+
+
+    /* mip frame */
+    require('./layout');
+    require('./fixed-element');
+    var viewport = require('./viewport');
+    require('./customElement');
+    var registerMipElement = require('./element');
+    require('./util');
+    var resources = require('./resources');
+    var viewer = require('./viewer');
+
+    /* builtin components */
+    require('./components/mip-img');
+    require('./components/mip-pix');
+    require('./components/mip-carousel');
+    require('./components/mip-iframe');
+    require('./components/mip-video');
+    var components = require('./components/index');
+
     // Register builtin animaters
     animation.register();
-        
+
     // The global variable of MIP
-    !window.MIP && (window.MIP = {});
-    MIP.css = {};
-    MIP.registerMipElement = registerMipElement;
-    MIP.viewer = viewer;
-    MIP.viewport = viewport;
-    MIP.prerenderElement = resources.prerenderElement;
+    var Mip = {};
+    if (!window.MIP) {
+        window.MIP = Mip;
+    }
+
+    Mip.css = {};
+    Mip.registerMipElement = registerMipElement;
+    Mip.viewer = viewer;
+    Mip.viewport = viewport;
+    Mip.prerenderElement = resources.prerenderElement;
 
     // Initialize viewer
     viewer.init();
@@ -24,4 +63,8 @@ require(['./element', './components/index',
 
     // Show page
     viewer.show();
+
+    return Mip;
 });
+
+require(['mip']);
