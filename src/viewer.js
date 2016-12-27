e7734881c414848080aa4a82e2a76e4e8043a4cf
wupeng10 <wupeng10@baidu.com>
2016-12-27 11:15:00 +0800

    update

--- src/viewer.js
+++ src/viewer.js
@@ -71,7 +71,7 @@ define(function (require) {
             // Fix iphone 5s UC and ios 9 safari bug. While the back button is clicked, the cached page has some problems.
             // So we are forced to load the page in iphone 5s UC and ios 9 safari.
             var iosVersion = platform.getOsVersion();
-            iosVersion = iosVersion.split('.')[0];
+            iosVersion = iosVersion ? iosVersion.split('.')[0] : '';
             var needBackReload = (iosVersion == '8' && platform.isUc() && screen.width === 320)
                 || (iosVersion == '9' && platform.isSafari());
             if (needBackReload) {
