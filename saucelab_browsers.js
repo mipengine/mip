40889c90a814fe8d205615b9275ff625bccaddc2
wupengFEX <smartfutureplayer@gmail.com>
2017-07-13 19:15:12 +0800

    modify browser list

--- saucelab_browsers.js
+++ saucelab_browsers.js
@@ -4,144 +4,53 @@
  */
 
 module.exports = {
-    'DartiumWithWebPlatform': {
-        base: 'Dartium',
-        flags: ['--enable-experimental-web-platform-features']
-    },
-    'ChromeNoSandbox': {
-        base: 'Chrome',
-        flags: ['--no-sandbox']
-    },
-    'SL_CHROME': {
+    sl_chrome: {
         base: 'SauceLabs',
         browserName: 'chrome',
-        version: '54'
+        platform: 'Windows 7'
     },
-    'SL_CHROMEBETA': {
+    sl_firefox: {
         base: 'SauceLabs',
-        browserName: 'chrome',
-        version: 'beta'
+        browserName: 'firefox'
     },
-    'SL_CHROMEDEV': {
-        base: 'SauceLabs',
-        browserName: 'chrome',
-        version: 'dev'
-    },
-    'SL_FIREFOX': {
-        base: 'SauceLabs',
-        browserName: 'firefox',
-        version: '50'
-    },
-    'SL_FIREFOXBETA': {
-        base: 'SauceLabs',
-        browserName: 'firefox',
-        version: 'beta'
-    },
-    'SL_FIREFOXDEV': {
-        base: 'SauceLabs',
-        browserName: 'firefox',
-        version: 'dev'
-    },
-    'SL_SAFARI7': {
+    sl_mac_safari: {
         base: 'SauceLabs',
         browserName: 'safari',
-        platform: 'OS X 10.9',
-        version: '7.0'
+        platform: 'OS X 10.10'
     },
-    'SL_SAFARI8': {
-        base: 'SauceLabs',
-        browserName: 'safari',
-        platform: 'OS X 10.10',
-        version: '8.0'
-    },
-    'SL_SAFARI9': {
-        base: 'SauceLabs',
-        browserName: 'safari',
-        platform: 'OS X 10.11',
-        version: '9.0'
-    },
-    'SL_SAFARI10': {
-        base: 'SauceLabs',
-        browserName: 'safari',
-        platform: 'OS X 10.12',
-        version: '10.0'
-    },
-    'SL_IOS7': {
-        base: 'SauceLabs',
-        browserName: 'iphone',
-        platform: 'OS X 10.10',
-        version: '7.1'
-    },
-    'SL_IOS8': {
-        base: 'SauceLabs',
-        browserName: 'iphone',
-        platform: 'OS X 10.10',
-        version: '8.4'
-    },
-    'SL_IOS9': {
-        base: 'SauceLabs',
-        browserName: 'iphone',
-        platform: 'OS X 10.10',
-        version: '9.3'
-    },
-    'SL_IOS10': {
-        base: 'SauceLabs',
-        browserName: 'iphone',
-        platform: 'OS X 10.10',
-        version: '10.0'
-    },
-    'SL_IE9': {
+    // ie family
+    sl_ie_9: {
         base: 'SauceLabs',
         browserName: 'internet explorer',
-        platform: 'Windows 2008',
+        platform: 'Windows 7',
         version: '9'
     },
-    'SL_IE10': {
+    sl_ie_10: {
         base: 'SauceLabs',
         browserName: 'internet explorer',
-        platform: 'Windows 2012',
+        platform: 'Windows 8',
         version: '10'
     },
-    'SL_IE11': {
+    sl_ie_11: {
         base: 'SauceLabs',
         browserName: 'internet explorer',
         platform: 'Windows 8.1',
         version: '11'
     },
-    'SL_EDGE': {
+    sl_edge: {
         base: 'SauceLabs',
         browserName: 'MicrosoftEdge',
-        platform: 'Windows 10',
-        version: '13.10586'
+        platform: 'Windows 10'
     },
-    'SL_ANDROID4.1': {
+    // mobile
+    sl_ios_safari_9: {
+        base: 'SauceLabs',
+        browserName: 'iphone',
+        version: '10.3'
+    },
+    sl_android_6_0: {
         base: 'SauceLabs',
         browserName: 'android',
-        platform: 'Linux',
-        version: '4.1'
-    },
-    'SL_ANDROID4.2': {
-        base: 'SauceLabs',
-        browserName: 'android',
-        platform: 'Linux',
-        version: '4.2'
-    },
-    'SL_ANDROID4.3': {
-        base: 'SauceLabs',
-        browserName: 'android',
-        platform: 'Linux',
-        version: '4.3'
-    },
-    'SL_ANDROID4.4': {
-        base: 'SauceLabs',
-        browserName: 'android',
-        platform: 'Linux',
-        version: '4.4'
-    },
-    'SL_ANDROID5': {
-        base: 'SauceLabs',
-        browserName: 'android',
-        platform: 'Linux',
-        version: '5.1'
+        version: '6.0'
     }
 }
