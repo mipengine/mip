86db98667966f8c06e181a810365598614f4cdde
wupengFEX <smartfutureplayer@gmail.com>
2017-07-14 12:57:09 +0800

    sauce browser

--- saucelab_browsers.js
+++ saucelab_browsers.js
@@ -13,29 +13,5 @@ module.exports = {
     sl_firefox: {
         base: 'SauceLabs',
         browserName: 'firefox'
-    },
-    // ie family
-    sl_ie_9: {
-        base: 'SauceLabs',
-        browserName: 'internet explorer',
-        platform: 'Windows 7',
-        version: '9'
-    },
-    sl_ie_10: {
-        base: 'SauceLabs',
-        browserName: 'internet explorer',
-        platform: 'Windows 8',
-        version: '10'
-    },
-    sl_ie_11: {
-        base: 'SauceLabs',
-        browserName: 'internet explorer',
-        platform: 'Windows 8.1',
-        version: '11'
-    },
-    // mobile
-    sl_android_6_0: {
-        base: 'SauceLabs',
-        browserName: 'android'
     }
 }
