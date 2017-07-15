7f4254dd132b96fe1236acb6d2cec488649ad207
wupengFEX <smartfutureplayer@gmail.com>
2017-07-16 00:44:18 +0800

    change unit test

--- test/dom/rect.js
+++ test/dom/rect.js
@@ -1,24 +1,32 @@
 define(function (require) {
     'use strict';
-
     var platform = require('utils/platform');
     platform.needSpecialScroll = true;
     var rect = require('dom/rect');
+    var docEle = document.body || document.documentElement;
+    var height = docEle.height;
     describe('rect', function () {
+        before(function () {
+            docEle.style.height = '10000px';
+        });
+
+        after(function () {
+            docEle.style.height = height;
+        });
+
         it('getElementRect', function () {
             expect(rect.getElementRect(document.body).bottom).to.be.above(0);
         });
 
         it('getElementOffset', function () {
-            var docEle = document.body || document.documentElement;
+            var style = getComputedStyle(docEle);
             expect(rect.getElementOffset(docEle).left).to.be.above(0);
             expect(rect.getElementOffset(docEle).top).to.be.above(0);
-            expect(rect.getElementOffset(docEle).width).to.be.above(Math.round(docEle.style.width));
-            expect(rect.getElementOffset(docEle).height).to.be.above(Math.round(docEle.style.height));
+            expect(rect.getElementOffset(docEle).width).to.be.least(Math.round(parseInt(style.width, 10)));
+            expect(rect.getElementOffset(docEle).height).to.be.least(Math.round(parseInt(style.height, 10)));
         });
 
         it('setScrollTop', function () {
-            document.body.style.height = '10000px';
             rect.setScrollTop(500);
             expect(rect.getScrollTop()).to.be.equal(500);
         });
