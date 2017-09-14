e0cab4fde7f46cf6e3b449e12e880b448705d612
wupengFEX <smartfutureplayer@gmail.com>
2017-09-14 19:23:06 +0800

    rebuild mip img extension

--- src/components/mip-img.js
+++ src/components/mip-img.js
@@ -1,21 +1,168 @@
 /**
- * @file mip-img 图片组件
- * @author wangpei07
+ * @file MIP Image extension
+ * @author xxx
+ * @modify wupeng10 2017-9-14 Refactoring code and add unit test
  */
-
 define(function (require) {
     'use strict';
 
+    // include util function
     var util = require('util');
-    var customElem = require('customElement').create();
-    var Gesture = util.Gesture;
     var css = util.css;
     var rect = util.rect;
+    var Gesture = util.Gesture;
+    // include tools
     var naboo = require('naboo');
-    var viewport = require('viewport');
     var viewer = require('viewer');
+    var viewport = require('viewport');
+    // include customElement
+    var customElem = require('customElement').create();
 
-    function getPopupImgPos(imgWidth, imgHeight) {
+    /**
+     * Trigger when element into visible area
+     *
+     */
+    customElem.prototype.firstInviewCallback = function () {
+        var ele = this.element.querySelector('img');
+        if (!ele) {
+            // create image
+            var img = new Image();
+            this.applyFillContent(img, true);
+
+            // add attrributes on image
+            var e = this.element;
+            img.src = util.makeCacheUrl(e.getAttribute('src'), 'img');
+            if (e.getAttribute('alt')) {
+                img.setAttribute('alt', e.getAttribute('alt'));
+            }
+            e.appendChild(img);
+
+            // bind event
+            this.bindEvent(e, img);
+        }
+    };
+
+    /**
+     * Bind image element event
+     *
+     * @param {HTMLElement} element mip image element
+     * @param {HTMLElement} img image element
+     */
+    customElem.prototype.bindEvent = function (element, img) {
+        var self = this;
+        if (element.hasAttribute('popup')) {
+            img.addEventListener('click', function (e) {
+                self.popupHandle(e, element, img);
+            }, false);
+        }
+
+        img.addEventListener('load', function () {
+            element.classList.add('mip-img-loaded');
+            element.customElement.resourcesComplete();
+        }, false);
+
+        // Http header accept has 'image/webp', But browser don't support
+        // Set image visibility hidden in order to hidden extra style
+        self.errHandle = self.errorHandle.bind(self, img);
+        img.addEventListener('error', self.errHandle, false);
+    };
+
+    /**
+     * Trigger when image load error
+     *
+     * @param {HTMLElement} img image element
+     */
+    customElem.prototype.errorHandle =  function (img) {
+        if (!viewer.isIframed) {
+            return;
+        }
+        var ele = document.createElement('a');
+        ele.href = img.src;
+        ele.search = ele.search ? ele.search : '?';
+        ele.search += (/&$/.test(ele.search) ? '' : '&') + 'mip_img_ori';
+        img.src = ele.href;
+        img.removeEventListener('error', this.errHandle);
+    };
+
+    /**
+     * Image popup event handler
+     *
+     * @param {Object} event event
+     * @param {HTMLElement} element mip image element
+     * @param {HTMLElement} img image element
+     */
+    customElem.prototype.popupHandle = function (event, element, img) {
+        var self = this;
+        event.stopPropagation();
+
+        // if image has not load, then don't popup
+        if (img.width + img.naturalWidth === 0) {
+            return;
+        }
+
+        var popup = self.createPopup(img);
+        var popupBg = popup.querySelector('.mip-img-popUp-bg');
+        var popupImg = popup.querySelector('img');
+        var imgOffset = self.getImgOffset(img);
+
+        css(popupImg, imgOffset);
+        css(popupBg, 'opacity', 1);
+        css(popup, 'display', 'block');
+        naboo.animate(popupImg, self.getPopupImgPos(imgOffset.width, imgOffset.height)).start();
+        css(img, 'visibility', 'hidden');
+        css(img.parentNode, 'zIndex', 'inherit');
+
+        this.popHandle = this.popupClickHandle.bind(this, {
+            popupBg: popupBg,
+            popupImg: popupImg,
+            img: img,
+            popup: popup
+        });
+        popup.addEventListener('click', this.popHandle, false);
+
+        this.rzHandle = this.resizeHandle.bind(this, {
+            img: img,
+            popupImg: popupImg,
+            imgOffset: imgOffset
+        });
+        window.addEventListener('resize', this.rzHandle);
+    };
+
+    /**
+     * Popup image cilck handler
+     *
+     * @param {Object} opt params
+     */
+    customElem.prototype.popupClickHandle = function (opt) {
+        naboo.animate(opt.popupBg, {
+            opacity: 0
+        }).start();
+        naboo.animate(opt.popupImg, this.getImgOffset(opt.img)).start(function () {
+            css(opt.img, 'visibility', 'visible');
+            css(opt.popup, 'display', 'none');
+        });
+        opt.popup.removeEventListener('click', this.popHandle, false);
+    };
+
+    /**
+     * Resize event handler
+     *
+     * @param {Object} opt params
+     */
+    customElem.prototype.resizeHandle = function (opt) {
+        var imgOffset = this.getImgOffset(opt.img);
+        css(opt.popupImg, opt.imgOffset);
+        naboo.animate(opt.popupImg, this.getPopupImgPos(imgOffset.width, imgOffset.height)).start();
+    };
+
+    /**
+     * Get popup image position rect
+     *
+     * @param {number} imgWidth image width
+     * @param {number} imgHeight image height
+     * @return {Object} rect
+     */
+    customElem.prototype.getPopupImgPos = function (imgWidth, imgHeight) {
         var width = viewport.getWidth();
         var height = Math.round(width * imgHeight / imgWidth);
         var top = (viewport.getHeight() - height) / 2;
@@ -25,22 +172,32 @@ define(function (require) {
             left: 0,
             top: top
         };
-    }
+    };
 
-    var getImgOffset = function (img) {
+    /**
+     * Get image offset
+     *
+     * @param {HTMLElement} img image element
+     * @return {Object} offset
+     */
+    customElem.prototype.getImgOffset = function (img) {
         var imgOffset = rect.getElementOffset(img);
         return imgOffset;
     };
 
-    // 创建弹层 dom
-    function createPopup(element, img) {
+    /**
+     * Create popup
+     *
+     * @param {HTMLElement} img image element
+     * @return {HTMLElement} popup element
+     */
+    customElem.prototype.createPopup = function (img) {
         var mipPopWrap = document.querySelector('.mip-img-popUp-wrapper');
         if (!!mipPopWrap && mipPopWrap.getAttribute('data-name') === 'mip-img-popUp-name'
             && mipPopWrap.parentNode.tagName.toLowerCase() === 'body') {
             mipPopWrap.querySelector('img').setAttribute('src', img.src);
             return mipPopWrap;
         }
-
         var popup = document.createElement('div');
         // 阻止纵向滑动
         new Gesture(popup, {
@@ -49,9 +206,6 @@ define(function (require) {
         popup.className = 'mip-img-popUp-wrapper';
         popup.setAttribute('data-name', 'mip-img-popUp-name');
 
-        /*
-        * 创建图片预览图层
-        */
         var popUpBg = document.createElement('div');
         var innerImg = new Image();
 
@@ -64,96 +218,13 @@ define(function (require) {
         document.body.appendChild(popup);
 
         return popup;
-    }
-
-    function bindPopup(element, img) {
-        var popup;
-        var popupBg;
-        var popupImg;
-        // 图片点击时展现图片
-        img.addEventListener('click', function (event) {
-            event.stopPropagation();
-            // 图片未加载则不弹层
-            if (img.width + img.naturalWidth === 0) {
-                return;
-            }
-
-            var onResize = function () {
-                imgOffset = getImgOffset(img);
-                css(popupImg, imgOffset);
-                naboo.animate(popupImg, getPopupImgPos(imgOffset.width, imgOffset.height)).start();
-            };
-            window.addEventListener('resize', onResize);
-
-            popup = createPopup(element, img);
-            popupBg = popup.querySelector('.mip-img-popUp-bg');
-            popupImg = popup.querySelector('img');
-
-            popup.addEventListener('click', imagePop, false);
-
-            function imagePop() {
-                naboo.animate(popupBg, {
-                    opacity: 0
-                }).start();
-                naboo.animate(popupImg, getImgOffset(img)).start(function () {
-                    css(img, 'visibility', 'visible');
-                    css(popup, 'display', 'none');
-                });
-                popup.removeEventListener('click', imagePop, false);
-            }
-
-            var imgOffset = getImgOffset(img);
-
-            css(popupImg, imgOffset);
-            css(popupBg, 'opacity', 1);
-            css(popup, 'display', 'block');
-
-            naboo.animate(popupImg, getPopupImgPos(imgOffset.width, imgOffset.height)).start();
-            css(img, 'visibility', 'hidden');
-            css(img.parentNode, 'zIndex', 'inherit');
-        }, false);
-    }
-
-    var bindEvent = function (element, img) {
-        img.addEventListener('load', function () {
-            element.classList.add('mip-img-loaded');
-            element.customElement.resourcesComplete();
-        });
-
-        // Http header accept has 'image/webp', But browser don't support
-        // Set image visibility hidden in order to hidden extra style
-        img.addEventListener('error', function () {
-            if (viewer.isIframed) {
-                element.classList.add('mip-hidden');
-            }
-        });
     };
 
-    function firstInviewCallback() {
-        var ele = this.element.querySelector('img');
-        if (ele) {
-            return;
-        }
-        var _img = new Image();
-        this.applyFillContent(_img, true);
-        var ele = this.element;
-
-        var src = util.makeCacheUrl(ele.getAttribute('src'), 'img');
-        _img.src = src;
-
-        if (ele.getAttribute('alt')) {
-            _img.setAttribute('alt', ele.getAttribute('alt'));
-        }
-
-        ele.appendChild(_img);
-        if (ele.hasAttribute('popup')) {
-            bindPopup(ele, _img);
-        }
-
-        bindEvent(ele, _img);
-    }
-
-    customElem.prototype.firstInviewCallback = firstInviewCallback;
+    /**
+     * Has resource, extend from custom element
+     *
+     * @return {boolean} whether resource existes or not
+     */
     customElem.prototype.hasResources = function () {
         return true;
     };
---
+++ test/components/mip-img.js
@@ -0,0 +1,86 @@
+define(function (require) {
+    'use strict';
+
+    var util = require('util');
+    var Img = require('components/mip-img');
+    var img = new Img();
+    var cEvent = util.event.create('click');
+
+    describe('mip img', function () {
+        it('firstInviewCallback', function () {
+            var HTML = '<mip-img alt="mip img" popup layout="responsive" width="350" height="263" src="https://www.mipengine.org/static/img/sample_01.jpg"></mip-img>';
+            img.element = util.dom.create(HTML);
+            img.element.customElement = {resourcesComplete: function () {}};
+            img.firstInviewCallback();
+            var ele = img.element.querySelectorAll('img');
+            expect(ele.length).to.be.at.least(1);
+        });
+
+        it('img load error', function (done) {
+            var HTML = '<mip-img src="https://www.mipengine.org/static/img/sample_xx.jpg"></mip-img>';
+            img.element = util.dom.create(HTML);
+            img.element.customElement = {resourcesComplete: function () {}};
+            img.firstInviewCallback();
+            var ele = img.element.querySelector('img');
+            setTimeout(function () {
+                expect(ele.src.indexOf('mip_img_ori')).to.be.at.least(0);
+                done();
+            }, 500);
+        });
+
+        it('popupHandle', function (done) {
+            var HTML = '<mip-img alt="mip img" popup layout="responsive" width="350" height="263" src="https://www.mipengine.org/static/img/sample_01.jpg"></mip-img>';
+            img.element = util.dom.create(HTML);
+            img.element.customElement = {resourcesComplete: function () {}};
+            img.firstInviewCallback();
+
+            var ele = img.element.querySelector('img');
+            img.popupHandle(cEvent, img.element, ele);
+            setTimeout(function () {
+                var ele = document.body.querySelectorAll('.mip-img-popUp-wrapper');
+                expect(ele.length).to.be.at.least(1);
+                done();
+            }, 100);
+        });
+
+        it('popup hash exist', function (done) {
+            setTimeout(function () {
+                var HTML = '<mip-img alt="mip img" popup layout="responsive" width="350" height="263" src="https://www.mipengine.org/static/img/sample_01.jpg"></mip-img>';
+                img.element = util.dom.create(HTML);
+                img.element.customElement = {resourcesComplete: function () {}};
+                img.firstInviewCallback();
+
+                var ele = img.element.querySelector('img');
+                img.popupHandle(cEvent, img.element, ele);
+                var ele = document.body.querySelectorAll('.mip-img-popUp-wrapper');
+                expect(ele.length).to.be.at.least(1);
+                done();
+            }, 200);
+        });
+
+        it('popupClickHandle', function (done) {
+            var HTML = '<mip-img alt="mip img" popup layout="responsive" width="350" height="263" src="https://www.mipengine.org/static/img/sample_01.jpg"></mip-img>';
+            img.element = util.dom.create(HTML);
+            img.element.customElement = {resourcesComplete: function () {}};
+            img.firstInviewCallback();
+
+            setTimeout(function () {
+                var popup = document.querySelector('.mip-img-popUp-wrapper');
+                var popUpBg = popup.querySelector('.mip-img-popUp-bg');
+                var popupImg = popup.querySelector('img');
+                var ele = img.element.querySelector('img');
+                expect(popup).to.not.be.undefined;
+                expect(popUpBg).to.not.be.undefined;
+                expect(popupImg).to.not.be.undefined;
+                expect(ele).to.not.be.undefined;
+                img.popupClickHandle({
+                    popup: popup,
+                    popUpBg: popUpBg,
+                    popupImg: popupImg,
+                    img: ele
+                });
+                done();
+            }, 200);
+        });
+    });
+});
