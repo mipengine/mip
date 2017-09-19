07e75e82bc04d8569d8e56c994efa15c65ddc235
xuexb <fe.xiaowu@gmail.com>
2017-09-19 17:39:50 +0800

    update the test does not depend on compilation

--- scripts/unit/karma.base.conf.js
+++ scripts/unit/karma.base.conf.js
@@ -5,29 +5,10 @@
 
 module.exports = {
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '../../dist/',
+    basePath: '../../',
 
     // Important: 所有插件必须在此声明
-    plugins: [
-        // frameworks
-        'karma-mocha',
-        'karma-chai',
-        'karma-chai-as-promised',
-        //'karma-sinon',
-        'karma-chai-sinon',
-
-        // reporters
-        'karma-mocha-reporter',
-        'karma-coverage',
-        'karma-html-reporter',
-
-        // launchers
-        'karma-chrome-launcher',
-        'karma-sauce-launcher',
-
-        // coveralls
-        'karma-coveralls'
-    ],
+    plugins: ['karma-*'],
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
@@ -40,17 +21,20 @@ module.exports = {
         'deps/promise.js',
         'deps/document-register-element.max.js',
         'deps/esl.js',
-        'deps/zepto.js',
-        'test/index.js', {
+        'scripts/unit/require.config.js',
+        {
             pattern: 'src/**/*.js',
             included: false
-        }, {
+        },
+        {
             pattern: 'deps/**/*.js',
             included: false
-        }, {
+        },
+        {
             pattern: 'test/**/*.js',
             included: false
         },
+        'src/less/mip.css'
     ],
 
 
@@ -69,8 +53,22 @@ module.exports = {
         'src/**/*.js': ['coverage']
     },
 
-    htmlReporter: {
-        outputDir: './dist/test-result' // relative to cwd
+    // htmlReporter: {
+    //     outputDir: './dist/test-result' // relative to cwd
+    // },
+
+    coverageReporter: {
+        // specify a common output directory
+        dir: './',
+        reporters: [
+            {
+                type: 'lcov',
+                subdir: 'coverage'
+            },
+            {
+                type: 'text-summary'
+            }
+        ]
     },
 
     // enable / disable colors in the output (reporters and logs)
--- scripts/unit/karma.unit.conf.js
+++ scripts/unit/karma.unit.conf.js
@@ -7,19 +7,14 @@ var base = require('./karma.base.conf.js');
 
 module.exports = function(config) {
     var options = Object.assign(base, {
-        coverageReporter: {
-            dir: './test-coverage' // relative to basePath
-        },
-
-        // test results reporter to use
-        // possible values: 'dots', 'progress'
-        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['mocha', 'html', 'coverage'],
-
         // level of logging
         // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
         // Note: 如果要调试Karma，请设置为DEBUG
-        logLevel: config.LOG_INFO
+        logLevel: config.LOG_INFO,
+
+        reporters: [
+            'mocha'
+        ],
     });
 
     config.set(options);
