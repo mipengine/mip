3641ba1607c58f0c5fc5656fb70f801079e3752f
xuexb <fe.xiaowu@gmail.com>
2017-11-03 14:41:01 +0800

    feat: 添加本地测试多浏览 sauce

--- scripts/unit/karma.sauce.conf.js
+++ scripts/unit/karma.sauce.conf.js
@@ -29,20 +29,24 @@ var customLaunchers = {
     }
 };
 
+if (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY) {
+    console.error('---------------');
+    console.error('Make sure the SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables are set.');
+    console.error('---------------');
+    process.exit(1);
+}
+
 module.exports = function(config) {
-    var buildId =
-        'MIP TRAVIS #' + process.env.TRAVIS_BUILD_NUMBER;
+    var buildId = 'MIP TRAVIS #' + (process.env.TRAVIS_BUILD_NUMBER || Date.now());
     var options = Object.assign(base, {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: process.env.CI ? ['mocha', 'dots', 'saucelabs'] : ['mocha', 'progress', 'saucelabs'],
+        reporters: ['mocha', 'saucelabs'],
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: process.env.TRAVIS
-            ? base.browsers.concat(Object.keys(customLaunchers))
-            : base.browsers,
+        browsers: Object.keys(customLaunchers),
 
         // level of logging
         // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
@@ -59,10 +63,12 @@ module.exports = function(config) {
                 'no-ssl-bump-domains': 'all' // Ignore SSL error on Android emulator
             },
             build: buildId,
+            recordVideo: false,
+            public: 'public',
 
             // use travis + addons.jwt to connect
-            startConnect: false,
-            tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER
+            startConnect: !process.env.TRAVIS,
+            tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER || buildId
         }
     });
 
