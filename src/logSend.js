4bc2f168daf5ff3b3e7c2af21c29b1f746fb4693
schoeu <schoeu89@gmail.com>
2017-12-29 11:18:30 +0800

    add mip monitor.

---
+++ src/logSend.js
@@ -0,0 +1,26 @@
+/**
+ * @file logSend.js
+ * @description 数据上报处理
+ */
+
+define(function () {
+    'use strict';
+
+    function MipLog() {
+        this.data = {};
+    }
+
+    // 数据上报逻辑
+    MipLog.prototype.sendLog = function (type, msg) {
+        if (!type) {
+            return;
+        }
+        this.data.event = type;
+        this.data.data = msg || {};
+
+        if (window !== window.top) {
+            window.parent.postMessage(this.data, '*');
+        }
+    };
+    return new MipLog();
+});
